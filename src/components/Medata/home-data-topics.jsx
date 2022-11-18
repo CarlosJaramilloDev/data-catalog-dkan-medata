754759c1bde2e9b662b2e5472c18760120a3ca73
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-11-18 00:46:47 -0500

    Fix detail topics component

--- src/components/Medata/home-data-topics.jsx
+++ src/components/Medata/home-data-topics.jsx
@@ -11,7 +11,7 @@ const HomeDataTopics = ({
   const itemsShow = items.map((item, index) => {
     console.log(item);
     return (
-      <HomeDataTopicDetail id={i} url={item.ref} title={item.title} />
+      <HomeDataTopicDetail id={index} url={item.ref} title={item.title} />
     );
   })
   
@@ -22,56 +22,6 @@ const HomeDataTopics = ({
       <p class="text">
       {text}
       </p>
-      <div class="grid">
-        <div class="category-info">
-          <img src={IconoGubernamental} alt="Gubernamental" />
-          <p class="text">Gubernamental</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoSocial} alt="Social" />
-          <p class="text">Social</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoSalud} alt="Salud" />
-          <p class="text">Salud</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoPoblacion} alt="Población" />
-          <p class="text">Población</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoInfraestructura} alt="Infraestructura y Catastro" />
-          <p class="text">Infraestructura y Catastro</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoHacienda} alt="Hacienda, Presupuesto y Contratación" />
-          <p class="text">Hacienda, Presupuesto y Contratación</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoEducacion} alt="Educación, Cultura y Recreación" />
-          <p class="text">Educación, Cultura y Recreación</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoMedioAmbiente} alt="Medio Ambiente" />
-          <p class="text">Medio Ambiente</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoMovilidad} alt="Movilidad" />
-          <p class="text">Movilidad</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoPlanDesarrollo} alt="Plan de Desarrollo" />
-          <p class="text">Plan de Desarrollo</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoOrdenamiento} alt="Ordenamiento Territorial" />
-          <p class="text">Ordenamiento Territorial</p>
-        </div>
-        <div class="category-info">
-          <img src={IconoDesarrolloEconomico} alt="Desarrollo Económico" />
-          <p class="text">Desarrollo Económico</p>
-        </div>
-      </div>
       <div class="grid">
        {itemsShow}
       </div>
