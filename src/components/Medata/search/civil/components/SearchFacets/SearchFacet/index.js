20eb11abe5e503658df76afd03658c2b8d4007e1
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-12-07 01:27:07 -0500

    Test facets sidebar not show all

--- src/components/Medata/search/civil/components/SearchFacets/SearchFacet/index.js
+++ src/components/Medata/search/civil/components/SearchFacets/SearchFacet/index.js
@@ -3,13 +3,17 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports["default"] = void 0;
 
-var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
+var _slicedToArray2 = _interopRequireDefault(
+  require("@babel/runtime/helpers/slicedToArray")
+);
 
-var _toConsumableArray2 = _interopRequireDefault(require("@babel/runtime/helpers/toConsumableArray"));
+var _toConsumableArray2 = _interopRequireDefault(
+  require("@babel/runtime/helpers/toConsumableArray")
+);
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -29,26 +33,28 @@ var _search_functions = require("../../../services/search/search_functions");
 
 var _ToggleBlock = _interopRequireDefault(require("../../ToggleBlock"));
 
-var _ShowMoreContainer = _interopRequireDefault(require("../../ShowMoreContainer"));
+var _ShowMoreContainer = _interopRequireDefault(
+  require("../../ShowMoreContainer")
+);
 
 _fontawesomeSvgCore.library.add(_freeSolidSvgIcons.fas);
 
 var SearchFacet = function SearchFacet(_ref) {
   var facetType = _ref.facetType,
-      label = _ref.label,
-      facets = _ref.facets,
-      dispatch = _ref.dispatch,
-      selected = _ref.selected,
-      toggleClasses = _ref.toggleClasses,
-      InputComponent = _ref.InputComponent,
-      _ref$reset = _ref.reset,
-      resetActive = _ref$reset.active,
-      resetIcon = _ref$reset.icon,
-      selectedFacets = _ref.selectedFacets,
-      inputType = _ref.inputType,
-      onMore = _ref.onMore,
-      onLess = _ref.onLess;
-  var myLabel = '';
+    label = _ref.label,
+    facets = _ref.facets,
+    dispatch = _ref.dispatch,
+    selected = _ref.selected,
+    toggleClasses = _ref.toggleClasses,
+    InputComponent = _ref.InputComponent,
+    _ref$reset = _ref.reset,
+    resetActive = _ref$reset.active,
+    resetIcon = _ref$reset.icon,
+    selectedFacets = _ref.selectedFacets,
+    inputType = _ref.inputType,
+    onMore = _ref.onMore,
+    onLess = _ref.onLess;
+  var myLabel = "";
 
   if (!label) {
     myLabel = facetType;
@@ -59,10 +65,14 @@ var SearchFacet = function SearchFacet(_ref) {
   var choices = facets.map(function (item) {
     var itemName = item.name;
     var itemTotal = item.total;
-    var key = "".concat(facetType.toLowerCase(), "-").concat(itemName.replace(/\s/g, ''), "-").concat(Math.random() * 100);
-    var checked = selected.filter(function (selectedFacet) {
-      return selectedFacet === itemName;
-    }).length > 0 || false;
+    var key = ""
+      .concat(facetType.toLowerCase(), "-")
+      .concat(itemName.replace(/\s/g, ""), "-")
+      .concat(Math.random() * 100);
+    var checked =
+      selected.filter(function (selectedFacet) {
+        return selectedFacet === itemName;
+      }).length > 0 || false;
 
     var onChangeFunction = function onChangeFunction(e) {
       var value = e.target.value;
@@ -76,98 +86,157 @@ var SearchFacet = function SearchFacet(_ref) {
 
       var newFacet = [facetType, value];
 
-      if (inputType === 'radio') {
+      if (inputType === "radio") {
         return dispatch({
-          type: 'RESET_FACETS',
+          type: "RESET_FACETS",
           data: {
-            selectedFacets: [].concat((0, _toConsumableArray2["default"])(selectedFacets.filter(function (_ref2) {
-              var _ref3 = (0, _slicedToArray2["default"])(_ref2, 1),
-                  type = _ref3[0];
+            selectedFacets: [].concat(
+              (0, _toConsumableArray2["default"])(
+                selectedFacets.filter(function (_ref2) {
+                  var _ref3 = (0, _slicedToArray2["default"])(_ref2, 1),
+                    type = _ref3[0];
 
-              return type !== facetType;
-            })), [newFacet])
-          }
+                  return type !== facetType;
+                })
+              ),
+              [newFacet]
+            ),
+          },
         });
       }
 
       dispatch({
-        type: 'UPDATE_FACETS',
+        type: "UPDATE_FACETS",
         data: {
-          newFacet: newFacet
-        }
+          newFacet: newFacet,
+        },
       });
     };
 
     if (InputComponent) {
-      return /*#__PURE__*/_react["default"].createElement(InputComponent, {
-        key: key,
-        checked: checked,
-        name: facetType,
-        type: inputType,
-        value: itemName,
-        onChange: onChangeFunction
-      }, "".concat(itemName, " (").concat(itemTotal, ")"));
+      return /*#__PURE__*/ _react["default"].createElement(
+        InputComponent,
+        {
+          key: key,
+          checked: checked,
+          name: facetType,
+          type: inputType,
+          value: itemName,
+          onChange: onChangeFunction,
+        },
+        "".concat(itemName, " (").concat(itemTotal, ")")
+      );
     }
 
-    return /*#__PURE__*/_react["default"].createElement("div", {
-      className: "dc-facet-option",
-      key: key
-    }, /*#__PURE__*/_react["default"].createElement(_reactstrap.Input, {
-      checked: checked,
-      id: key,
-      name: facetType,
-      type: inputType,
-      value: itemName,
-      onChange: onChangeFunction
-    }), /*#__PURE__*/_react["default"].createElement(_reactstrap.Label, {
-      htmlFor: key,
-      className: 'text'
-    }, /*#__PURE__*/_react["default"].createElement(_reactFontawesome.FontAwesomeIcon, {
-      icon: ['fas', checked ? 'check-square' : 'square'],
-      size: "1x",
-      "aria-hidden": "true",
-      role: "presentation"
-    }), "".concat(itemName, " (").concat(itemTotal, ")")));
+    if (itemTotal > 0) {
+      return /*#__PURE__*/ _react["default"].createElement(
+        "div",
+        {
+          className: "dc-facet-option",
+          key: key,
+        },
+        /*#__PURE__*/ _react["default"].createElement(_reactstrap.Input, {
+          checked: checked,
+          id: key,
+          name: facetType,
+          type: inputType,
+          value: itemName,
+          onChange: onChangeFunction,
+        }),
+        /*#__PURE__*/ _react["default"].createElement(
+          _reactstrap.Label,
+          {
+            htmlFor: key,
+            className: "text",
+          },
+          /*#__PURE__*/ _react["default"].createElement(
+            _reactFontawesome.FontAwesomeIcon,
+            {
+              icon: ["fas", checked ? "check-square" : "square"],
+              size: "1x",
+              "aria-hidden": "true",
+              role: "presentation",
+            }
+          ),
+          "".concat(itemName, " (").concat(itemTotal, ")")
+        )
+      );
+    }
   });
-  return /*#__PURE__*/_react["default"].createElement("div", {
-    className: "dc-search-facet-container"
-  }, resetActive && !(0, _lodash.isEmpty)(selected) ? /*#__PURE__*/_react["default"].createElement("button", {
-    type: "button",
-    onClick: function onClick() {
-      return dispatch((0, _search_functions.resetSelectedFacets)(selectedFacets, facetType));
+  return /*#__PURE__*/ _react["default"].createElement(
+    "div",
+    {
+      className: "dc-search-facet-container",
     },
-    className: "facet-reset-button"
-  }, resetIcon && /*#__PURE__*/_react["default"].createElement("span", {
-    className: "undo-icon"
-  }, resetIcon), "Reset") : null, /*#__PURE__*/_react["default"].createElement(_ToggleBlock["default"], {
-    key: facetType // TODO: Fix this so it's adjustable
-    ,
-    title: /*#__PURE__*/_react["default"].createElement("span", null, myLabel),
-    headingClasses: "facet-block-".concat(facetType, "-inner").concat(toggleClasses ? " ".concat(toggleClasses) : ''),
-    innerClasses: "inner-".concat(facetType, "-facets")
-  }, /*#__PURE__*/_react["default"].createElement(_ShowMoreContainer["default"], {
-    container: "div",
-    items: choices,
-    limit: 10,
-    onMore: onMore,
-    onLess: onLess
-  })));
+    resetActive && !(0, _lodash.isEmpty)(selected)
+      ? /*#__PURE__*/ _react["default"].createElement(
+          "button",
+          {
+            type: "button",
+            onClick: function onClick() {
+              return dispatch(
+                (0, _search_functions.resetSelectedFacets)(
+                  selectedFacets,
+                  facetType
+                )
+              );
+            },
+            className: "facet-reset-button",
+          },
+          resetIcon &&
+            /*#__PURE__*/ _react["default"].createElement(
+              "span",
+              {
+                className: "undo-icon",
+              },
+              resetIcon
+            ),
+          "Reset"
+        )
+      : null,
+    /*#__PURE__*/ _react["default"].createElement(
+      _ToggleBlock["default"],
+      {
+        key: facetType, // TODO: Fix this so it's adjustable
+        title: /*#__PURE__*/ _react["default"].createElement(
+          "span",
+          null,
+          myLabel
+        ),
+        headingClasses: "facet-block-"
+          .concat(facetType, "-inner")
+          .concat(toggleClasses ? " ".concat(toggleClasses) : ""),
+        innerClasses: "inner-".concat(facetType, "-facets"),
+      },
+      /*#__PURE__*/ _react["default"].createElement(
+        _ShowMoreContainer["default"],
+        {
+          container: "div",
+          items: choices,
+          limit: 10,
+          onMore: onMore,
+          onLess: onLess,
+        }
+      )
+    )
+  );
 };
 
 SearchFacet.defaultProps = {
-  label: '',
+  label: "",
   selected: [],
   toggleClasses: null,
   InputComponent: null,
   selectedFacets: [],
   reset: {
-    active: false
+    active: false,
   },
-  inputType: 'checkbox'
+  inputType: "checkbox",
 };
 SearchFacet.propTypes = {
   facetType: _propTypes["default"].string.isRequired,
-  facets: _propTypes["default"].arrayOf(_propTypes["default"].arrayOf).isRequired,
+  facets: _propTypes["default"].arrayOf(_propTypes["default"].arrayOf)
+    .isRequired,
   dispatch: _propTypes["default"].func.isRequired,
   label: _propTypes["default"].string,
   selected: _propTypes["default"].arrayOf(_propTypes["default"].arrayOf),
@@ -176,11 +245,11 @@ SearchFacet.propTypes = {
   selectedFacets: _propTypes["default"].arrayOf(_propTypes["default"].array),
   reset: _propTypes["default"].shape({
     active: _propTypes["default"].bool,
-    icon: _propTypes["default"].func
+    icon: _propTypes["default"].func,
   }),
   inputType: _propTypes["default"].string,
   onMore: _propTypes["default"].func,
-  onLess: _propTypes["default"].func
+  onLess: _propTypes["default"].func,
 };
 var _default = SearchFacet;
 exports["default"] = _default;
--- src/templates/not_found/index.js
+++ src/templates/not_found/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <h1>Page not found.</h1>
+    <h1>Página no encontrada.</h1>
   );
 }
 
