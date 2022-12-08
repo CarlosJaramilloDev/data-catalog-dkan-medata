03a7f528db9f250fd838d4babd7d96ca6aa5fa44
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-12-08 00:32:53 -0500

    Change png to svg icons Att2

--- src/components/Medata/home/home-social-media.jsx
+++ src/components/Medata/home/home-social-media.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import IconFacebook from '../../../medatatheme/assets/icons/social-network/twitter.png';
+import IconFacebook from '../../../medatatheme/assets/icons/social-network/facebook.png';
 import IconTwitter from '../../../medatatheme/assets/icons/social-network/twitter.png';
 import IconInstagram from '../../../medatatheme/assets/icons/social-network/insta.png';
 import IconVimeo from '../../../medatatheme/assets/icons/social-network/vimeo.png';
@@ -11,31 +11,31 @@ const HomeSocialMedia = () => {
     <div class="social-container">
       <div class="social-item">
         <img src={IconFacebook} alt="Facebook" />
-        <a href="/">
+        <a href="https://www.facebook.com/AlcaldiadeMed">
           {">"} Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconTwitter} alt="Twitter" />
-        <a href="/">
+        <a href="https://twitter.com/AlcaldiadeMed">
           {">"} Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconInstagram} alt="Instagram" />
-        <a href="/">
+        <a href="https://www.instagram.com/alcaldiademed">
           {">"} Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconVimeo} alt="Vimeo" />
-        <a href="/">
+        <a href="https://vimeo.com/user118558690">
           {">"} Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconYoutube} alt="Youtube" />
-        <a href="/">
+        <a href="https://www.youtube.com/channel/UCAFLU70jPz4bODq_5BSWPIg">
           {">"} Seguir ahora
         </a>
       </div>
--- src/components/Medata/search/civil/templates/TopicIcon/index.js
+++ src/components/Medata/search/civil/templates/TopicIcon/index.js
@@ -1,31 +1,5 @@
 "use strict";
 
-var IconoGubernamental = require("../../../../../../medatatheme/assets/icons/topics/icon-1.png");
-var IconoSocial = require("../../../../../../medatatheme/assets/icons/topics/icon-2.png");
-var IconoSalud = require("../../../../../../medatatheme/assets/icons/topics/icon-3.png");
-var IconoPoblacion = require("../../../../../../medatatheme/assets/icons/topics/icon-4.png");
-var IconoInfraestructura = require("../../../../../../medatatheme/assets/icons/topics/icon-5.png");
-var IconoHacienda = require("../../../../../../medatatheme/assets/icons/topics/icon-6.png");
-var IconoEducacion = require("../../../../../../medatatheme/assets/icons/topics/icon-7.png");
-var IconoMedioAmbiente = require("../../../../../../medatatheme/assets/icons/topics/icon-8.png");
-var IconoMovilidad = require("../../../../../../medatatheme/assets/icons/topics/icon-9.png");
-var IconoPlanDesarrollo = require("../../../../../../medatatheme/assets/icons/topics/icon-10.png");
-var IconoOrdenamiento = require("../../../../../../medatatheme/assets/icons/topics/icon-11.png");
-var IconoDesarrolloEconomico = require("../../../../../../medatatheme/assets/icons/topics/icon-12.png");
-
-var IconoGubernamentalBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-1.png");
-var IconoSocialBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-2.png");
-var IconoSaludBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-3.png");
-var IconoPoblacionBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-4.png");
-var IconoInfraestructuraBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-5.png");
-var IconoHaciendaBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-6.png");
-var IconoEducacionBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-7.png");
-var IconoMedioAmbienteBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-8.png");
-var IconoMovilidadBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-9.png");
-var IconoPlanDesarrolloBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-10.png");
-var IconoOrdenamientoBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-11.png");
-var IconoDesarrolloEconomicoBlack = require("../../../../../../medatatheme/assets/icons/topics/icon-12.png");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
@@ -40,61 +14,63 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 function TopicIcon(_ref) {
   var title = _ref.title,
     height = _ref.height,
-    width = _ref.width,
-    color = _ref.color;
+    width = _ref.width;
 
-  var icon = null;
+  var iconClass = null;
 
   switch (title) {
-    case "Social": 
-      icon = (color === 'white') ? IconoSocial : IconoSocialBlack;
+    case "Social":
+      iconClass = 'social';
       break;
     case "Población":
-      icon = (color === 'white') ? IconoPoblacion : IconoPoblacionBlack;
+      iconClass = 'poblacion';
       break;
     case "Plan de Desarrollo":
-      icon = (color === 'white') ? IconoPlanDesarrollo : IconoPlanDesarrolloBlack;
+      iconClass = 'desarrollo';
       break;
     case "Movilidad":
-      icon = (color === 'white') ? IconoMovilidad : IconoMovilidadBlack;
+      iconClass = 'movilidad';
       break;
     case "Infraestructura y Catastro":
-      icon = (color === 'white') ? IconoInfraestructura : IconoInfraestructuraBlack;
+      iconClass = 'infraestructura';
       break;
     case "Educación, Cultura y Recreación":
-      icon = (color === 'white') ? IconoEducacion : IconoEducacionBlack;
+      iconClass = 'educacion';
       break;
     case "Hacienda, Presupuesto y Contratación":
-      icon = (color === 'white') ? IconoHacienda : IconoHaciendaBlack;
+      iconClass = 'hacienda';
       break;
     case "Desarrollo Económico":
-      icon = (color === 'white') ? IconoDesarrolloEconomico : IconoDesarrolloEconomicoBlack;
+      iconClass = 'economia';
       break;
     case "Salud":
-      icon = (color === 'white') ? IconoSalud : IconoSaludBlack;
+      iconClass = 'salud';
       break;
     case "Gubernamental":
-      icon = (color === 'white') ? IconoGubernamental : IconoGubernamentalBlack;
+      iconClass = 'gubernamental';
       break;
     case "Ordenamiento Territorial":
-      icon = (color === 'white') ? IconoOrdenamiento : IconoOrdenamientoBlack;
+      iconClass = 'ordenamiento';
       break;
     case "Medio Ambiente":
-      icon = (color === 'white') ? IconoMedioAmbiente : IconoMedioAmbienteBlack;
+      iconClass = 'ambiente';
       break;
-    case "Vivienda, Ciudad y Territorio":
-      icon = (color === 'white') ? IconoOrdenamiento : IconoOrdenamientoBlack;
+    case "Cultura":
+      iconClass = 'cultura';
+      break;
+    case "Territorios":
+      iconClass = 'territorios';
+      break;
+    case "Vivienda":
+      iconClass = 'vivienda';
       break;
     default:
-      icon = (color === 'white') ? IconoDesarrolloEconomico : IconoDesarrolloEconomicoBlack;
+      iconClass = 'social';
       break;
   }
 
-  return /*#__PURE__*/ _react["default"].createElement("img", {
-    src: icon,
-    alt: title,
-    width: width,
-    height: height,
+  return /*#__PURE__*/ _react["default"].createElement("i", {
+    className: `category-icon icon-${iconClass}`
   });
 }
 
