b744fd761b224b261fe97df6b15644725e31f153
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-12-22 00:17:52 -0500

    Clean code breadcrumb

--- src/components/Medata/breadcrumb.jsx
+++ src/components/Medata/breadcrumb.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 
-const Breadcrumb = () => {
+const Breadcrumb = ({
+  themes = []
+}) => {
+  const [breadcrumbExtra, setBreadcrumbExtra] = React.useState([]);
+  React.useEffect(() => {
+    const templatesToAddToBreadcrumb = [
+      'publisher__name=Medell%C3%8Dn%20en%20Cifras'
+    ]
+    
+    function getUrl() {
+      return window.location.href;
+    }
+
+    const currectUrl = getUrl();
+    const elementNewArray = [];
+
+    if (typeof currectUrl !== 'undefined') {  
+      templatesToAddToBreadcrumb.forEach((element, index) => {
+        if (currectUrl.includes(element)) {
+          let newElement = (<li className="breadcrumb-item" key={index}>
+            <a className="breadcrumb-link" href={`/search?${element}`}>Medellín en cifras</a>
+          </li>);
+          elementNewArray.push(newElement);
+        }
+      });
+    };
+
+    if(themes.length > 0){
+
+    }
+
+
+    setBreadcrumbExtra(elementNewArray);
+
+  }, [breadcrumbExtra]);
   return (
     <nav className="breadcrumb">
       <ol className="breadcrumb-list">
@@ -12,9 +46,9 @@ const Breadcrumb = () => {
         <li className="breadcrumb-item">
           <a className="breadcrumb-link" href="/">Medata</a>
         </li>
-        <li className="breadcrumb-item">
-          <a className="breadcrumb-link" href="/search">Búsqueda</a>
-        </li>
+        {breadcrumbExtra.length === 0 ? <li className="breadcrumb-item">
+          <a className="breadcrumb-link" href="/search">Datos</a>
+        </li> : breadcrumbExtra}
       </ol>
     </nav>
   );
