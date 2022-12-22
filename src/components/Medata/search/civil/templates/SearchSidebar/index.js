174af331e4615e75c501e7a5b929d2fa7b3cb87f
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-12-22 02:10:00 -0500

    Ad search title

--- src/components/Medata/search/civil/templates/SearchSidebar/index.js
+++ src/components/Medata/search/civil/templates/SearchSidebar/index.js
@@ -22,6 +22,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var SearchSidebar = function SearchSidebar(_ref) {
+  var title = _ref.title;
 
   var _useContext = (0, _react.useContext)(_search_defaults.SearchDispatch),
       searchState = _useContext.searchState,
@@ -32,7 +33,7 @@ var SearchSidebar = function SearchSidebar(_ref) {
       loading = searchState.loading;
   return /*#__PURE__*/_react["default"].createElement("div", {
     className: "dc-search-sidebar sidenav"
-  }, /*#__PURE__*/_react["default"].createElement("div", {
+  }, /*#__PURE__*/_react["default"].createElement("p", null, title), /*#__PURE__*/_react["default"].createElement("div", {
     className: "dc-search-sidebar-options"
   }, loading ? /*#__PURE__*/_react["default"].createElement(_reactContentLoader.BulletList, null) : facetsResults && facetsResults.length && /*#__PURE__*/_react["default"].createElement(_SearchFacets["default"], {
     facetsConfig: defaultFacets,
