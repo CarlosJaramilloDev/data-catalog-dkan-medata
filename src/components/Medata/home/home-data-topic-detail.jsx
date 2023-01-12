b68c36a31223d96d281131d424373499bc1d6d17
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2023-01-12 00:09:34 -0500

    Change icons React Topics class

--- src/components/Medata/home/home-data-topic-detail.jsx
+++ src/components/Medata/home/home-data-topic-detail.jsx
@@ -7,53 +7,112 @@ const HomeDataTopicDetail = ({
 }) => {
   let iconClass;
   switch (title) {
-    case "Social":
-      iconClass = 'social';
+    case "Agricultura y Desarrollo Rural":
+      iconClass = 'icon-01';
       break;
-    case "Población":
-      iconClass = 'poblacion';
+    
+    case "Ambiente y Desarrollo Sostenible":
+      iconClass = 'icon-02';
       break;
-    case "Plan de Desarrollo":
-      iconClass = 'desarrollo';
+    
+    case "Ciencia Tecnología e Innovación":
+      iconClass = 'icon-03';
       break;
-    case "Movilidad":
-      iconClass = 'movilidad';
+    
+    case "Comercio, Industria y Turismo":
+      iconClass = 'icon-04';
       break;
-    case "Infraestructura y Catastro":
-      iconClass = 'infraestructura';
-      break;
-    case "Educación, Cultura y Recreación":
-      iconClass = 'educacion';
-      break;
-    case "Hacienda, Presupuesto y Contratación":
-      iconClass = 'hacienda';
-      break;
-    case "Desarrollo Económico":
-      iconClass = 'economia';
-      break;
-    case "Salud":
-      iconClass = 'salud';
-      break;
-    case "Gubernamental":
-      iconClass = 'gubernamental';
-      break;
-    case "Ordenamiento Territorial":
-      iconClass = 'ordenamiento';
-      break;
-    case "Medio Ambiente":
-      iconClass = 'ambiente';
-      break;
+    
     case "Cultura":
-      iconClass = 'cultura';
+      iconClass = 'icon-05';
       break;
-    case "Territorios":
-      iconClass = 'territorios';
+    
+    case "Deporte y Recreación":
+      iconClass = 'icon-06';
       break;
-    case "Vivienda":
-      iconClass = 'vivienda';
+    
+    case "Economía y Finanzas":
+      iconClass = 'icon-07';
       break;
+    
+    case "Educación":
+      iconClass = 'icon-08';
+      break;
+    
+    case "Estadísticas Nacionales":
+      iconClass = 'icon-09';
+      break;
+    
+    case "Función Pública":
+      iconClass = 'icon-10';
+      break;
+    
+    case "Gastos Gubernamentales":
+      iconClass = 'icon-11';
+      break;
+    
+    case "Hacienda y Crédito Público":
+      iconClass = 'icon-12';
+      break;
+    
+    case "Inclusión Social y Reconciliación":
+      iconClass = 'icon-13';
+      break;
+    
+    case "Justicia y Derecho":
+      iconClass = 'icon-14';
+      break;
+    
+    case "Mapas Nacionales":
+      iconClass = 'icon-15';
+      break;
+    
+    case "Minas y Energías":
+      iconClass = 'icon-16';
+      break;
+    
+    case "Ordenamiento Territorial":
+      iconClass = 'icon-17';
+      break;
+    
+    case "Organismos de Control":
+      iconClass = 'icon-18';
+      break;
+    
+    case "Participación Ciudadana":
+      iconClass = 'icon-19';
+      break;
+    
+    case "Presupuestos Gubernamentales":
+      iconClass = 'icon-20';
+      break;
+    
+    case "Resultados Electorales":
+      iconClass = 'icon-21';
+      break;
+    
+    case "Salud y Protección Social":
+      iconClass = 'icon-22';
+      break;
+    
+    case "Seguridad y Defensa":
+      iconClass = 'icon-23';
+      break;
+    
+    case "Trabajo":
+      iconClass = 'icon-24';
+      break;
+    
+    case "Transporte":
+      iconClass = 'icon-25';
+      break;
+    
+    case "Vivienda, Ciudad y Territorio":
+      iconClass = 'icon-26';
+      break;
+    
     default:
-      iconClass = 'desarrollo';
+      iconClass = 'icon-15';
       break;
   }
 
