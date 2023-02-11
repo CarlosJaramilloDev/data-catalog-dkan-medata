1626887bfc14ac110fa70c242f16f1dcbf36830a
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2023-02-11 11:59:05 -0500

    Fix home arrow

--- src/components/Medata/home/home-social-media.jsx
+++ src/components/Medata/home/home-social-media.jsx
@@ -4,6 +4,7 @@ import IconTwitter from '../../../medatatheme/assets/icons/social-network/twitte
 import IconInstagram from '../../../medatatheme/assets/icons/social-network/insta.png';
 import IconVimeo from '../../../medatatheme/assets/icons/social-network/vimeo.png';
 import IconYoutube from '../../../medatatheme/assets/icons/social-network/youtube.png';
+import IconArrow from '../../../medatatheme/assets/icons/social-arrow.png';
 
 const HomeSocialMedia = () => {
 
@@ -12,31 +13,31 @@ const HomeSocialMedia = () => {
       <div class="social-item">
         <img src={IconFacebook} alt="Facebook" />
         <a href="https://www.facebook.com/AlcaldiadeMed">
-          {">"} Seguir ahora
+        <img src={IconArrow} /> Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconTwitter} alt="Twitter" />
         <a href="https://twitter.com/AlcaldiadeMed">
-          {">"} Seguir ahora
+          <img src={IconArrow} /> Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconInstagram} alt="Instagram" />
         <a href="https://www.instagram.com/alcaldiademed">
-          {">"} Seguir ahora
+          <img src={IconArrow} /> Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconVimeo} alt="Vimeo" />
         <a href="https://vimeo.com/user118558690">
-          {">"} Seguir ahora
+          <img src={IconArrow} /> Seguir ahora
         </a>
       </div>
       <div class="social-item">
         <img src={IconYoutube} alt="Youtube" />
         <a href="https://www.youtube.com/channel/UCAFLU70jPz4bODq_5BSWPIg">
-          {">"} Seguir ahora
+          <img src={IconArrow} /> Seguir ahora
         </a>
       </div>
     </div>
